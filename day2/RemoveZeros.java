baa834b5399e73af60166bb17c1efc022f7b22bf
thanya <thaniyakc@gmail.com>
2020-10-24 12:54:49 +0530

    day2 removezero with counting function

--- day2/RemoveZeros.java
+++ day2/RemoveZeros.java
@@ -6,10 +6,31 @@ public class RemoveZeros {
     }
   }
 
-  public static int[] removeZeros(int[] numberList) {}
+  public static int countNonZero(int[] list) {
+    int count = 0;
+    for (int number : list) {
+      if (number != 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public static int[] removeZeros(int[] numberList) {
+    int length = countNonZero(numberList);
+    int[] removedArray = new int[length];
+    int newArrayIndex = 0;
+    for (int index = 0; index < numberList.length; index++) {
+      if (numberList[index] != 0) {
+        removedArray[newArrayIndex] = numberList[index];
+        newArrayIndex++;
+      }
+    }
+    return removedArray;
+  }
 
   public static void main(String[] args) {
-    int[] list1 = { 3, 5, 2, 8, 9 };
+    int[] list1 = { 3, 5, 0, 2, 8, 0, 9 };
 
     printIntegerList(removeZeros(list1));
   }
